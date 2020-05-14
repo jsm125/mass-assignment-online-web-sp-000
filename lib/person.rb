53982217230b8e5f1d69e7efaa41f4492c3bb19c
Matt Im <matthew.seiim@gmail.com>
2020-05-14 16:20:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
   #your code here
+  attr_accessor :name, :complexion, :pant_length
+  
+  def initialize(attributes)
+    attributes.each {|key,value|, self.send(("#(key)=")), value)}
+  end
+  
+  
 end
